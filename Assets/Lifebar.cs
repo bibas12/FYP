9cc606908befd10e53bbd775b6f75099d6df7890
bibas12 <90327712+bibas12@users.noreply.github.com>
2023-03-12 10:40:35 +0545

    update

--- Assets/Lifebar.cs
+++ Assets/Lifebar.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 public class LifeBar : MonoBehaviour
 {
     float rm;
-    GameObject needle;
+    GameObject Needle;
+
     // Start is called before the first frame update
     void Start()
     {
-        needle = transform.Find("needle").gameObject;
+        Needle = transform.Find("Needle").gameObject; // Find the needle game object by name
+        Debug.Log("Needle object found: " + (Needle != null)); // Debug statement to check if the needle object was found
     }
 
     // Update is called once per frame
     void Update()
     {
-        rm = PlayerPrefs.GetFloat("LifeBar"); // use GetFloat instead of GetInt since LifeMeter is a float
+        rm = PlayerPrefs.GetInt("LifeBar");
 
-        needle.transform.localPosition = new Vector3((rm - 25) / 25, 0, 0);
+        if (Needle != null) // Check if the needle game object was found
+        {
+            Needle.transform.localPosition = new Vector3((rm - 25) / 25, 0, 0);
+        }
+        else
+        {
+            Debug.LogWarning("Needle object not found!"); // Debug statement if the needle game object was not found
+        }
     }
 }
