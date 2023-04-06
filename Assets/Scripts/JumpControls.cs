6fc1096a99ddb98b1648e60cf1691d185563b41a
bibas12 <bibashrai81@gmail.com>
2023-04-06 15:48:51 +0545

    lvl190 complete

--- Assets/Scripts/JumpControls.cs
+++ Assets/Scripts/JumpControls.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class JumpControls : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator animator;
     public float jumpForce = 5f;
     public float maxYPosition = 0.08f; // Maximum y position
     public KeyCode KeyToPress;
@@ -11,6 +12,7 @@ public class JumpControls : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = Vector2.zero; // Set initial velocity to zero
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -18,10 +20,12 @@ public class JumpControls : MonoBehaviour
         if (Input.GetKeyDown(KeyToPress) && transform.position.y <= maxYPosition)
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            animator.SetBool("isJumping", true);
         }
         else if (transform.position.y > maxYPosition)
         {
             rb.velocity = Vector2.zero; // Reset velocity to zero
+            animator.SetBool("isJumping", false);
         }
     }
 }
