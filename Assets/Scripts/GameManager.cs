f2d8c680943385f49ea2bf8a919c12382da20d7a
bibas12 <bibashrai81@gmail.com>
2023-04-07 12:48:34 +0545

    lvl almost complete

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,113 +1,121 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
-{
-    public AudioSource theMusic;
-
-    public bool startPlaying;
-
-    public BeatScroller theBS;
-
-    public static GameManager instance;
-
-    public int currentScore;
-    public int scorePerNote = 100;
-    public int scorePerGoodNote = 125;
-    public int scorePerPerfectNote = 150;
-
-    public Text scoreText;
-    public Text multiText;
-
-    public int currentMultiplier;
-    public int multiplierTracker;
-    public int[] multiplierThresholds;
-
-    public float totalNotes;
-    public float normalHits;
-    public float goodHits;
-    public float perfectHits;
-    public float missedHits;
-
-    public GameObject resultsScreen;
-    public Text percentHitText, normalsText, goodText, perfectText, missesText, rankText, finalScoreText;
-
-    // Start is called before the first frame update
-    void Start()
+    public class GameManager : MonoBehaviour
     {
-        instance = this;
+        public AudioSource theMusic;
 
-        scoreText.text = "Score: 0";
-        currentMultiplier = 1;
+        public bool startPlaying;
 
-        totalNotes = FindObjectsOfType<NoteObject>().Length;
-    }
+        public BeatScroller theBS;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (!startPlaying)
+        public static GameManager instance;
+
+        public int currentScore;
+        public int scorePerNote = 100;
+        public int scorePerGoodNote = 125;
+        public int scorePerPerfectNote = 150;
+
+        public Text scoreText;
+        public Text multiText;
+        public Text consecutiveMissedNotesText;
+
+
+        public int currentMultiplier;
+        public int multiplierTracker;
+        public int[] multiplierThresholds;
+
+        public float totalNotes;
+        public float normalHits;
+        public float goodHits;
+        public float perfectHits;
+        public float missedHits;
+
+        public int consecutiveMissedNotes;
+
+
+        public GameObject resultsScreen;
+        public Text percentHitText, normalsText, goodText, perfectText, missesText, rankText, finalScoreText;
+
+        // Start is called before the first frame update
+        void Start()
         {
-            if (Input.anyKeyDown)
-            {
-                startPlaying = true;
-                theBS.hasStarted = true;
+            instance = this;
 
-                theMusic.Play();
-            }
+            scoreText.text = "Score: 0";
+            currentMultiplier = 1;
+
+            totalNotes = FindObjectsOfType<NoteObject>().Length;
         }
-        else
+
+        // Update is called once per frame
+        void Update()
         {
-            if (!theMusic.isPlaying && !resultsScreen.activeInHierarchy)
+            if (!startPlaying)
             {
-                resultsScreen.SetActive(true);
-
-                normalsText.text = "" + normalHits;
-                goodText.text = goodHits.ToString();
-                perfectText.text = perfectHits.ToString();
-                missesText.text = "" + missedHits;
-
-                float totalHit = normalHits + goodHits + perfectHits;
-                float percentHit = (totalHit / totalNotes) * 100f;
-
-                percentHitText.text = percentHit.ToString("F1") + "%";
-
-                string rankVal = "F";
-
-                if (percentHit > 40)
+                if (Input.anyKeyDown)
                 {
-                    rankVal = "D";
-                    if (percentHit > 55)
-                    {
-                        rankVal = "C";
-                        if (percentHit > 70)
-                        {
-                            rankVal = "B";
-                            if(percentHit > 85)
-                            {
-                                rankVal = "A";
-                                if (percentHit > 95)
-                                {
-                                    rankVal = "S";
-                                }
-                            }
+                    startPlaying = true;
+                    theBS.hasStarted = true;
 
+                    theMusic.Play();
+                }
+            }
+            else
+            {
+                if (!theMusic.isPlaying && !resultsScreen.activeInHierarchy)
+                {
+                    resultsScreen.SetActive(true);
+
+                    normalsText.text = "" + normalHits;
+                    goodText.text = goodHits.ToString();
+                    perfectText.text = perfectHits.ToString();
+                    missesText.text = "" + missedHits;
+
+                    float totalHit = normalHits + goodHits + perfectHits;
+                    float percentHit = (totalHit / totalNotes) * 100f;
+
+                    percentHitText.text = percentHit.ToString("F1") + "%";
+
+                    string rankVal = "F";
+
+                    if (percentHit > 40)
+                    {
+                        rankVal = "D";
+                        if (percentHit > 55)
+                        {
+                            rankVal = "C";
+                            if (percentHit > 70)
+                            {
+                                rankVal = "B";
+                                if(percentHit > 85)
+                                {
+                                    rankVal = "A";
+                                    if (percentHit > 95)
+                                    {
+                                        rankVal = "S";
+                                    }
+                                }
+
+                            }
                         }
                     }
+
+                    rankText.text = rankVal;
+
+                    finalScoreText.text = currentScore.ToString();
                 }
-
-                rankText.text = rankVal;
-
-                finalScoreText.text = currentScore.ToString();
             }
         }
-    }
 
     public void NoteHit()
     {
         Debug.Log("Hit On Time");
+
+        // Reset the consecutive missed notes counter
+        consecutiveMissedNotes = 0;
 
         if (multiplierThresholds.Length == 0 || currentMultiplier - 1 >= multiplierThresholds.Length)
         {
@@ -123,55 +131,106 @@ public class GameManager : MonoBehaviour
                 multiplierTracker = 0;
                 currentMultiplier++;
             }
-
-            
         }
 
         multiText.text = "Multiplier: x" + currentMultiplier;
-
-        //currentScore += scorePerNote * currentMultiplier;
-
-        //Debug.Log("Current multiplier: " + currentMultiplier);
-        //Debug.Log("Multiplier tracker: " + multiplierTracker);
-        //Debug.Log("Multiplier thresholds: " + string.Join(", ", multiplierThresholds));
-
-
         scoreText.text = "Score: " + currentScore;
+        consecutiveMissedNotesText.text = "Misses: " + consecutiveMissedNotes;
     }
+
 
     public void NormalHit()
-    {
-        currentScore += scorePerNote * currentMultiplier;
-        NoteHit();
-        Debug.Log("Normal Hit");
+        {
+            currentScore += scorePerNote * currentMultiplier;
+            NoteHit();
+            Debug.Log("Normal Hit");
 
-        normalHits++;
+            normalHits++;
+        }
+
+        public void GoodHit()
+        {
+            currentScore += scorePerGoodNote * currentMultiplier;
+            NoteHit();
+            goodHits++;
+        }
+
+        public void PerfectHit()
+        {
+            currentScore += scorePerPerfectNote * currentMultiplier;
+            NoteHit();
+            perfectHits++;
+        }
+
+
+        public void NoteMissed()
+        {
+            Debug.Log("Missed Note");
+
+            consecutiveMissedNotes++;
+            consecutiveMissedNotesText.text = "Misses: " + consecutiveMissedNotes;
+
+        if (consecutiveMissedNotes >= 4)
+            {
+                // Call a game over function to end the game
+                GameOver();
+                Debug.Log("GameLost");
+            }
+            else
+            {
+                currentMultiplier = 1;
+                multiplierTracker = 0;
+
+                multiText.text = "Multiplier: x" + currentMultiplier;
+
+                missedHits++;
+            }
+        }
+
+        void GameOver()
+        {
+            // Pause the music
+            theMusic.Pause();
+
+            // Display a game over screen or perform any other actions you want
+            resultsScreen.SetActive(true);
+
+            normalsText.text = "" + normalHits;
+            goodText.text = goodHits.ToString();
+            perfectText.text = perfectHits.ToString();
+            missesText.text = "" + missedHits;
+
+            float totalHit = normalHits + goodHits + perfectHits;
+            float percentHit = (totalHit / totalNotes) * 100f;
+
+            percentHitText.text = percentHit.ToString("F1") + "%";
+
+            string rankVal = "F";
+
+            if (percentHit > 40)
+            {
+                rankVal = "D";
+                if (percentHit > 55)
+                {
+                    rankVal = "C";
+                    if (percentHit > 70)
+                    {
+                        rankVal = "B";
+                        if (percentHit > 85)
+                        {
+                            rankVal = "A";
+                            if (percentHit > 95)
+                            {
+                                rankVal = "S";
+                            }
+                        }
+                    }
+                }
+            }
+
+            rankText.text = rankVal;
+
+            finalScoreText.text = currentScore.ToString();
+        }
+
     }
-
-    public void GoodHit()
-    {
-        currentScore += scorePerGoodNote * currentMultiplier;
-        NoteHit();
-        goodHits++;
-    }
-
-    public void PerfectHit()
-    {
-        currentScore += scorePerPerfectNote * currentMultiplier;
-        NoteHit();
-        perfectHits++;
-    }
-
-
-    public void NoteMissed()
-    {
-        Debug.Log("Missed Note");
-
-        currentMultiplier = 1;
-        multiplierTracker = 0;
-
-        multiText.text = "Multiplier: x" + currentMultiplier;
-
-        missedHits++;
-    }
-}
---
+++ Assets/Scripts/GameOverOnKeyPress.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class GameOverOnKeyPress : MonoBehaviour
+{
+    public KeyCode keyToPress;
+    public bool canBePressed;
+    private bool noteHit;
+
+    public GameObject hitEffect, perfectEffect;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(keyToPress))
+        {
+            if (canBePressed && !noteHit)
+            {
+                noteHit = true;
+                gameObject.SetActive(false);
+
+                Debug.Log("Special Note Hit");
+                GameManager.instance.consecutiveMissedNotes = 4; // immediately lose the game
+                GameObject effect = Instantiate(perfectEffect, transform.position, perfectEffect.transform.rotation);
+                Destroy(effect, 0.5f); // Destroy after 0.5 seconds
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Activator")
+        {
+            canBePressed = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Activator" && !noteHit)
+        {
+            canBePressed = false;
+        }
+    }
+}
